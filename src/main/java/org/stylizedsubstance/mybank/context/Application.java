d4ea026eeca03c2fe4eae273e2b67e3f048efb84
stylized-substance <116166387+stylized-substance@users.noreply.github.com>
2025-12-17 13:03:42 +0200

    added Application

---
+++ src/main/java/org/stylizedsubstance/mybank/context/Application.java
@@ -0,0 +1,7 @@
+package org.stylizedsubstance.mybank.context;
+
+import org.stylizedsubstance.mybank.service.TransactionService;
+
+public class Application {
+    public static final TransactionService transactionService = new TransactionService();
+}
